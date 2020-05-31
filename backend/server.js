c6ba2049420c5e07fb6e61a8e1fb5f2b1779c897
Chaeran Nam <chaerannam@Chaerans-MacBook-Pro.local>
2020-05-30 23:40:15 -0600

    try

---
+++ backend/server.js
@@ -0,0 +1,63 @@
+const express= require('express');
+//const cors = require('cors');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+
+//require('dotenv').config();
+
+const app=express();
+// Bodyparser middleware
+app.use(
+	bodyParser.urlencoded({
+	  extended: false
+	})
+  );
+  app.use(bodyParser.json());
+
+  // DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+/*app.use(cors());
+app.use(express.json());
+
+const uri =process.env.ATLAS_URI;
+mongoose.connect(uri,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true});
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+	console.log("MongoDb connection setup successfully");	
+});
+*/
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+/*const techsRouter= require('./routes/techs');
+const usersRouter= require('./routes/users');
+
+app.use('/techs',techsRouter);
+app.use('/users',usersRouter);
+
+app.listen(port, ()=>{
+	console.log(`Server running on Port: ${port}`);
+});*/
---
+++ src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+        </div>
+      </div>
+    );
+  }
+}export default Landing;
